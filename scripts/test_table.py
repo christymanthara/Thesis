50222ce611e75cbb4e12fdf6b9654cb2d7adcbfe
CHRISTY JO MANTHARA <100943241+christymanthara@users.noreply.github.com>
2025-04-11 16:03:41 +0200

    test ci/cd

--- scripts/test_table.py
+++ scripts/test_table.py
@@ -11,6 +11,7 @@ my_table = update_bioinf_table(my_table, 'lung', 'source 1', 'Tabula Muris')
 my_table = update_bioinf_table(my_table, 'kidney', 'Pavlin_tsne', 0.95)
 my_table = update_bioinf_table(my_table, 'pancreas', 'baron_', 0.95)
 my_table = update_bioinf_table(my_table, 'pancreas', 'source 1', 'Xen')
+my_table = update_bioinf_table(my_table, 'pancreas', 'source 1', 'test')
 
 # Step 3: Save updated data
 save_table_as_csv(my_table)
