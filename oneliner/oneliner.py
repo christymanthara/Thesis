9476f97ba642c248f4615176adfa3c1bac8479e3
CHRISTY JO MANTHARA <100943241+christymanthara@users.noreply.github.com>
2025-06-17 19:05:06 +0200

    Create oneliner.py

---
+++ oneliner/oneliner.py
@@ -0,0 +1,30 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# from plotting_with_knn import compute_tsne_embeddings
+from scripts.data_utils import preprocessing
+# from batch-effect-analysis import UCE.compute_uce_embeddings
+from scripts.compute_embeddings_scvi_scanvi_uce import compute_embeddings_scvi_scanvi_uce as ce
+from scripts import knn_plot_test
+
+from anndata import AnnData
+if __name__ == "__main__":
+    # 1.Loading and preprocessing the datasets
+    combined_data = preprocessing.load_and_preprocess_multi_embedder(
+        
+    file1="F:/Thesis/Datasets/baron_2016h.h5ad", 
+    file2="F:/Thesis/segerstolpe_transformed.h5ad",
+    save=True,          # Saves all files
+    split_output=False   # Getting the combined AnnData object
+)
+    # 2.Adding embeddings using scVI, scANVI, and UCE
+    embedded_adata = ce(combined_data)
+    
+    # 3.Run the knn plot test
+    knn_plot_test.compute_knn_tsne_all(embedded_adata, skip_preprocessing=True, n_jobs=1)
+    
+    
+    
+    
